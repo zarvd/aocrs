1a8bc5f7e5120ab7a5b7531af5ef482c923c59fc
Jialun Cai <jialun.cai@pm.me>
2024-12-19 13:18:23 +1100

    Refactor UT

--- src/year2023/mod.rs
+++ src/year2023/mod.rs
@@ -15,35 +15,33 @@ pub fn run(day: u64, part: u64, input: String) -> String {
 
     type SolveFn = fn(String) -> String;
     let handlers = [
-        ("1_1", day1::solve_part1 as SolveFn),
-        ("1_2", day1::solve_part2 as SolveFn),
-        ("2_1", day2::solve_part1 as SolveFn),
-        ("2_2", day2::solve_part2 as SolveFn),
-        ("3_1", day3::solve_part1 as SolveFn),
-        ("3_2", day3::solve_part2 as SolveFn),
-        ("4_1", day4::solve_part1 as SolveFn),
-        ("4_2", day4::solve_part2 as SolveFn),
-        ("5_1", day5::solve_part1 as SolveFn),
-        ("5_2", day5::solve_part2 as SolveFn),
-        ("6_1", day6::solve_part1 as SolveFn),
-        ("6_2", day6::solve_part2 as SolveFn),
-        ("7_1", day7::solve_part1 as SolveFn),
-        ("7_2", day7::solve_part2 as SolveFn),
-        ("8_1", day8::solve_part1 as SolveFn),
-        ("8_2", day8::solve_part2 as SolveFn),
-        ("9_1", day9::solve_part1 as SolveFn),
-        ("9_2", day9::solve_part2 as SolveFn),
-        ("10_1", day10::solve_part1 as SolveFn),
-        ("10_2", day10::solve_part2 as SolveFn),
-        ("11_1", day11::solve_part1 as SolveFn),
-        ("11_2", day11::solve_part2 as SolveFn),
+        ((1, 1), day1::solve_part1 as SolveFn),
+        ((1, 2), day1::solve_part2 as SolveFn),
+        ((2, 1), day2::solve_part1 as SolveFn),
+        ((2, 2), day2::solve_part2 as SolveFn),
+        ((3, 1), day3::solve_part1 as SolveFn),
+        ((3, 2), day3::solve_part2 as SolveFn),
+        ((4, 1), day4::solve_part1 as SolveFn),
+        ((4, 2), day4::solve_part2 as SolveFn),
+        ((5, 1), day5::solve_part1 as SolveFn),
+        ((5, 2), day5::solve_part2 as SolveFn),
+        ((6, 1), day6::solve_part1 as SolveFn),
+        ((6, 2), day6::solve_part2 as SolveFn),
+        ((7, 1), day7::solve_part1 as SolveFn),
+        ((7, 2), day7::solve_part2 as SolveFn),
+        ((8, 1), day8::solve_part1 as SolveFn),
+        ((8, 2), day8::solve_part2 as SolveFn),
+        ((9, 1), day9::solve_part1 as SolveFn),
+        ((9, 2), day9::solve_part2 as SolveFn),
+        ((10, 1), day10::solve_part1 as SolveFn),
+        ((10, 2), day10::solve_part2 as SolveFn),
+        ((11, 1), day11::solve_part1 as SolveFn),
+        ((11, 2), day11::solve_part2 as SolveFn),
     ]
     .into_iter()
-    .map(|(i, f)| (i.to_owned(), f))
     .collect::<HashMap<_, _>>();
 
-    let id = format!("{}_{}", day, part);
-    handlers.get(&id).unwrap()(input)
+    handlers.get(&(day, part)).unwrap()(input)
 }
 
 #[cfg(test)]
@@ -53,54 +51,97 @@ mod tests {
 
     use super::run;
 
+    fn run_test_with_dataset(day: u64, part: u64) {
+        let dir = format!("./dataset/2023/day{day}/part{part}");
+        assert!(fs::metadata(&dir).is_ok(), "dataset not found");
+
+        let input_path = format!("{dir}/{}.input", part - 1);
+        let output_path = format!("{dir}/{}.output", part - 1);
+        let input =
+            fs::read_to_string(&input_path).expect(&format!("read test input: {input_path}"));
+        let output =
+            fs::read_to_string(&output_path).expect(&format!("read test output: {output_path}"));
+
+        let start = Instant::now();
+        let actual = run(day, part, input);
+        let elapsed = start.elapsed();
+        assert_eq!(
+            actual,
+            output.trim(),
+            "Test for year 2023 - day {day} - part {part} failed"
+        );
+        println!(
+            "Running test for year 2023 - day {:0>2} - part {}: {}ms / {}ns",
+            day,
+            part,
+            elapsed.as_millis(),
+            elapsed.as_nanos()
+        );
+    }
+
     #[test]
-    fn test_run() {
-        let mut day = 1;
-        while fs::metadata(format!("./dataset/2023/day{day}")).is_ok() {
-            let mut part = 1;
-            while fs::metadata(format!("./dataset/2023/day{day}/part{part}")).is_ok() {
-                let num_tests = {
-                    let mut rv = -1;
-                    while fs::metadata(format!(
-                        "./dataset/2023/day{day}/part{part}/{}.input",
-                        rv + 1
-                    ))
-                    .is_ok()
-                    {
-                        rv += 1;
-                    }
-                    rv + 1
-                };
+    fn test_day1() {
+        run_test_with_dataset(1, 1);
+        run_test_with_dataset(1, 2);
+    }
 
-                for i in 0..num_tests {
-                    let input =
-                        fs::read_to_string(format!("./dataset/2023/day{day}/part{part}/{i}.input"))
-                            .expect("read test input");
-                    let output = fs::read_to_string(format!(
-                        "./dataset/2023/day{day}/part{part}/{i}.output"
-                    ))
-                    .expect("read test output");
+    #[test]
+    fn test_day2() {
+        run_test_with_dataset(2, 1);
+        run_test_with_dataset(2, 2);
+    }
 
-                    let start = Instant::now();
-                    let actual = run(day, part, input);
-                    let elapsed = start.elapsed();
-                    assert_eq!(
-                        actual, output,
-                        "Test #{i} for year 2023 - day {day} - part {part} failed",
-                    );
-                    println!(
-                        "Running #{}/{} tests for year 2023 - day {:0>2} - part {}: {}ms / {}ns",
-                        i + 1,
-                        num_tests,
-                        day,
-                        part,
-                        elapsed.as_millis(),
-                        elapsed.as_nanos()
-                    );
-                }
-                part += 1;
-            }
-            day += 1;
-        }
+    #[test]
+    fn test_day3() {
+        run_test_with_dataset(3, 1);
+        run_test_with_dataset(3, 2);
+    }
+
+    #[test]
+    fn test_day4() {
+        run_test_with_dataset(4, 1);
+        run_test_with_dataset(4, 2);
+    }
+
+    #[test]
+    fn test_day5() {
+        run_test_with_dataset(5, 1);
+        run_test_with_dataset(5, 2);
+    }
+
+    #[test]
+    fn test_day6() {
+        run_test_with_dataset(6, 1);
+        run_test_with_dataset(6, 2);
+    }
+
+    #[test]
+    fn test_day7() {
+        run_test_with_dataset(7, 1);
+        run_test_with_dataset(7, 2);
+    }
+
+    #[test]
+    fn test_day8() {
+        run_test_with_dataset(8, 1);
+        run_test_with_dataset(8, 2);
+    }
+
+    #[test]
+    fn test_day9() {
+        run_test_with_dataset(9, 1);
+        run_test_with_dataset(9, 2);
+    }
+
+    #[test]
+    fn test_day10() {
+        run_test_with_dataset(10, 1);
+        run_test_with_dataset(10, 2);
+    }
+
+    #[test]
+    fn test_day11() {
+        run_test_with_dataset(11, 1);
+        run_test_with_dataset(11, 2);
     }
 }
